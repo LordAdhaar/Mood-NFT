2fbf148cf3b420dc580159f77bb200d106455188
Adhaar Jain <adhaarjain@Adhaars-MacBook-Air.local>
2024-11-18 04:42:32 +0530

    Deploy MoodNFTs with flip functionality

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,42 @@
-// //SPDX-License-Identifier: MIT
-// pragma solidity 0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
 
-// import {Script} from "../lib/forge-std/src/Script.sol";
-// import {MoodNFT} from "../src/MoodNFT.sol";
-// import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
-// contract MoodNFT is Script {}
+contract MintMoodNFT is Script {
+    MoodNFT public moodNFT;
+
+    function run() external {
+        moodNFT = MoodNFT(
+            DevOpsTools.get_most_recent_deployment("MoodNFT", block.chainid)
+        );
+        mintNFT();
+    }
+
+    function mintNFT() public {
+        vm.startBroadcast();
+        moodNFT.mintNFT();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNFT is Script {
+    MoodNFT public moodNFT;
+    uint256 public constant TOKEN_ID_TO_FLIP = 0;
+
+    function run() external {
+        moodNFT = MoodNFT(
+            DevOpsTools.get_most_recent_deployment("MoodNFT", block.chainid)
+        );
+
+        flipMoodNFT();
+    }
+
+    function flipMoodNFT() public {
+        vm.startBroadcast();
+        moodNFT.flipMood(TOKEN_ID_TO_FLIP);
+        vm.stopBroadcast();
+    }
+}
